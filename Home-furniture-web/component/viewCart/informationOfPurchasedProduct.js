858709766349c26397c0e4eb2d92b16be49ca2d0
Công Lưu <mac@Konjluus-MacBook-Pro.local>
2022-01-08 19:05:51 +0700

    17th cm-konjluu

--- Home-furniture-web/component/viewCart/informationOfPurchasedProduct.js
+++ Home-furniture-web/component/viewCart/informationOfPurchasedProduct.js
@@ -11,30 +11,47 @@ export default class InformationOfPurchasedProduct {
 
     constructor(items) {
         this.informationOfPurchasedProduct_Container = document.createElement("div");
+        this.informationOfPurchasedProduct_Container.setAttribute('class'," flex  mt-20")
 
         this.itemDeleteButton = document.createElement("button");
 
+        this.product = document.createElement("div")
+        this.product.setAttribute('class','w-40 content-center')
+
         this.illustration = document.createElement("img");
         this.illustration.src=items.img;
+        this.illustration.setAttribute(
+            "class",
+            "w-10 h-10 object-cover hover:scale-110 duration-300"
+        );
+        
 
         this.nameProduct = document.createElement("p");
         this.nameProduct.textContent=items.name;
 
         this.cost = document.createElement("p");
         this.cost.textContent=`${items.cost}`;
+        this.cost.setAttribute('class','ml-10 w-40')
 
         this.quantily = document.createElement("input");
-        this.quantily.value=items.quantily
-        this.quantily.textContent=items.quantily
+        this.quantily.value=items.quantily;
+        this.quantily.textContent=items.quantily;
+        this.quantily.setAttribute(
+            "class",
+            "w-40 ml-10 border border-gray-500");
 
         this.total = document.createElement("p");
         this.total.textContent=items.total;
+        this.total.setAttribute('class','ml-10 w-40')
     }
 
     render(container) {
         this.informationOfPurchasedProduct_Container.appendChild(this.itemDeleteButton);
-        this.informationOfPurchasedProduct_Container.appendChild(this.illustration);
-        this.informationOfPurchasedProduct_Container.appendChild(this.nameProduct);
+        this.product.appendChild(this.nameProduct)
+        this.product.appendChild(this.illustration)
+
+        this.informationOfPurchasedProduct_Container.appendChild(this.product);
+        // this.informationOfPurchasedProduct_Container.appendChild(this.nameProduct);
         this.informationOfPurchasedProduct_Container.appendChild(this.cost);
         this.informationOfPurchasedProduct_Container.appendChild(this.quantily);
         this.informationOfPurchasedProduct_Container.appendChild(this.total);
--- Home-furniture-web/component/viewCart/viewCardChild.js
+++ Home-furniture-web/component/viewCart/viewCardChild.js
@@ -27,26 +27,33 @@ export default class ViewCardChild{
     constructor() {
         
         this.$viewCardChildContainer=document.createElement("div");
+        this.$viewCardChildContainer.setAttribute('class', "flex flex-col w-2/3 mx-auto")
 
 
         this.$parameter=document.createElement("div");
+        this.$parameter.setAttribute('class','flex mt-20')
 
         this.$nameProduct=document.createElement("p");
         this.$nameProduct.textContent="Sản phẩm";
+        this.$nameProduct.setAttribute('class','w-40')
 
         this.$cost=document.createElement("p");
         this.$cost.textContent="Giá";
+        this.$cost.setAttribute('class','ml-10 w-40')
 
         this.$quantily=document.createElement("p");
         this.$quantily.textContent="Số lượng";
+        this.$quantily.setAttribute('class','ml-10 w-40')
 
         this.$total=document.createElement("p");
         this.$total.textContent="Tổng";
+        this.$total.setAttribute('class','ml-10 w-40')
 
         this.$informationOfPurchasedProduct=document.createElement("div");
         // this.$informationOfPurchasedProduct=new InformationOfPurchasedProduct();
 
         this.$updateViewCart=document.createElement("div");
+        this.$updateViewCart.setAttribute('class', "mt-20")
 
         this.$codeSale=document.createElement("input");
         this.$codeSale.type="";
